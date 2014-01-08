cf415a4df09511afbe24efa2c67b1949868a24a2
chrisbuttery <info@chrisbuttery.com>
2014-01-08 21:21:24 +1100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# gcse
+
+  Google Custom Search Element tag.
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install chrisbuttery/gcse
+
+## API
+
+  require('chrisbuttery/gcse');
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * Expose `gsce`
+ */
+
+module.exports = gsce;
+
+/**
+ * Initialize `gsce`
+ *
+ * @param {String} id
+ */
+
+function gsce(id) {
+  load(url(id));
+}
+
+/**
+ * Get the url
+ */
+function url(id) {
+  return ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + 'google.com/cse/cse.js?cx=' + id;
+}
+
+/**
+ * Load the GSCE script tag
+ *
+ * @param {String} url
+ */
+
+function load(url) {
+  var first = document.getElementsByTagName('script')[0];
+  var js = document.createElement('script');
+  js.type = 'text/javascript';
+  js.async = true;
+  js.src = url;
+  first.parentNode.insertBefore(js, first);
+}
